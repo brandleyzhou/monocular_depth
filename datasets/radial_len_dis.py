6cd7ff95cd2e02077329695be4d6cb6b2a29a739
hang <brandleyzhou@gmail.com>
2020-09-08 17:42:14 +0100

    update distortion

--- datasets/radial_len_dis.py
+++ datasets/radial_len_dis.py
@@ -1,5 +1,8 @@
 import numpy as np
 import PIL.Image as Image
+import torch.nn.functional as functional
+import torch
+
 img_path = 'cups.png' 
 
 def apply_distortion(y,x, k1, k2):
@@ -13,43 +16,38 @@ def distorted_ordinates(y, x, h, w):
     # distortion function
     k1, k2 = 0.05,0.1
     y_d, x_d = apply_distortion(y_n,x_n,k1,k2) 
-    y_d = round((y_d + 1)/2 * h) 
-    x_d = round((x_d + 1)/2 * w)
-    if y_d <= h-1 and x_d <= w-1:
-        return y_d,x_d
-    else:
-        return y,x
+    #y_d = round((y_d + 1)/2 * h) 
+    #x_d = round((x_d + 1)/2 * w)
+    #if y_d <= h-1 and x_d <= w-1:
+    #    return y_d,x_d
+    #else:
+    #    return y,x
+    return y_d,x_d
 
 def distorting_img(img_path):
-    img = np.array(Image.open(img_path)).astype(np.float64)
+    img = np.array(Image.open(img_path)).astype(np.float32)
     print('img.shape:',img.shape)
     print('img.min:',img.min())
     print('img.max:',img.max())
     h,w,_ = img.shape
     img_d = np.zeros(img.shape)
+    grid = torch.zeros(h,w,2)
     for y in range(h):
         for x in range(w):
-            img_d[distorted_ordinates(y,x,h,w)] = img[y,x]
+            #img_d[distorted_ordinates(y,x,h,w)] = img[y,x]
+            grid[y][x][1],grid[y][x][0] = distorted_ordinates(y,x,h,w)
+    grid = grid.unsqueeze(0)
+    img = torch.from_numpy(img).permute(2,0,1).unsqueeze(0)
+    img_d = functional.grid_sample(img,grid,mode='bilinear',padding_mode='zeros')
     
-    for y in range(1,h-1):
-        for x in range(1,w-1):
-            if img_d[y,x].sum() == 0:
-                img_d[y,x] = 0.25 * img_d[y+1,x] + img_d[y-1,x] + img_d[y,x+1] +img_d[y,x-1]
+    #for y in range(1,h-1):
+    #    for x in range(1,w-1):
+    #        if img_d[y,x].sum() == 0:
+    #            img_d[y,x] = 0.25 * img_d[y+1,x] + img_d[y-1,x] + img_d[y,x+1] +img_d[y,x-1]
     
-    j = 0
-    for y in range(h):
-        for x in range(w):
-            if img[y,x].sum() == 0:
-                j+=1
-    print('img\'s zero_pixel sum:',j)
+    img_d = img_d.squeeze().permute(1,2,0).detach().cpu().numpy()
+    Image.fromarray(img_d.astype(np.uint8)).save('img_d6.png')
 
-    i = 0
-    for y in range(h):
-        for x in range(w):
-            if img_d[y,x].sum() == 0:
-                i+=1
-    print('img_d\'s zero_pixel sum:',i)
-    Image.fromarray(img_d.astype(np.uint8)).save('imge_d6.png')
-
+   
 if __name__ == '__main__':
     distorting_img(img_path)
