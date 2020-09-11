42f286b56f37d32b48c1fb3044219b9a814e854f
hang <brandleyzhou@gmail.com>
2020-09-11 12:46:38 +0100

    update

--- datasets/kitti_dataset.py
+++ datasets/kitti_dataset.py
@@ -43,8 +43,8 @@ class KITTIDataset(MonoDataset):
                 color = color.transpose(pil.FLIP_LEFT_RIGHT)
             elif data_augment == 'rotation':
                 color = functional.rotate(color,180)
-        if random() <= 0.5:
-            color = distorting_img(color)
+        #if random() <= 0.5:
+        #    color = distorting_img(color)
         return color
 
 
